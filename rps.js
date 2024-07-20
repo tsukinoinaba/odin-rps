218dd61259f1557c1d83776a6080ffaf074f90e9
Aria Ho <ariaho99@proton.me>
2024-07-20 12:43:41 +0800

    Add getComputerChoice function

--- rps.js
+++ rps.js
@@ -1,1 +1,14 @@
-console.log("Hello sekai");
+function getComputerChoice() {
+  let randomNumber = Math.floor(Math.random() * 3);
+  if (randomNumber === 0) {
+    return "rock";
+  }
+  else if (randomNumber === 1) {
+    return "paper";
+  }
+  return "scissors";
+}
+
+for (let i = 0; i < 5; i++) {
+  console.log(getComputerChoice());
+}
