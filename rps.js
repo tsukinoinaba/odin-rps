a9ce43c3a918cd41837732478b8660392ef814c5
Aria Ho <ariaho99@proton.me>
2024-07-20 12:58:48 +0800

    Add logic to play one round of RPS

--- rps.js
+++ rps.js
@@ -14,10 +14,43 @@ function getComputerChoice() {
 
 function getHumanChoice() {
   let humanChoice = prompt("What is your move? rock / paper / scissors");
+  humanChoice = humanChoice.toLowerCase();
   while (humanChoice !== "rock" && humanChoice !== "paper" && humanChoice !== "scissors") {
-    humanChoice = prompt("Please enter only \"rock\", \"paper\" or \"scissors\", without the quotes");
+    humanChoice = prompt("Please enter only \"rock\", \"paper\" or \"scissors\", without the quotes (case insensitive");
   }
   return humanChoice;
 }
 
-console.log(getHumanChoice());
+function playRound(computerChoice, humanChoice) {
+  let winner = 0;
+
+  console.log("Opponent plays " + computerChoice + "!");
+  console.log("You play " + humanChoice + "!");
+
+  if (humanChoice === "rock" && computerChoice === "scissors" ||
+      humanChoice === "paper" && computerChoice === "rock" ||
+      humanChoice === "scissors" && computerChoice === "paper") {
+    winner = 1;
+  }
+  else if (computerChoice === "rock" && playerChoice === "scissors" ||
+           computerChoice === "paper" && playerChoice === "rock" ||
+           computerChoice === "scissors" && playerChoice === "paper") {
+    winner = -1;
+  }
+
+  if (winner === 1) {
+    console.log("You win! " + humanChoice + " beats " + computerChoice + "!");
+    humanScore++;
+  }
+  else if (winner === -1) {
+    console.log("Opponent wins! " + computerChoice + " beats " + humanChoice + "!");
+    computerScore++;
+  }
+  else {
+    console.log("Tie!");
+  }
+}
+
+const computerChoice = getComputerChoice();
+const humanChoice = getHumanChoice();
+playRound(computerChoice, humanChoice);
